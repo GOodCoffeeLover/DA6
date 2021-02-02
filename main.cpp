d318f35eb785598d44f6d546de7834e227678be2
student31415 <qwerty65k@mail.ru>
2021-02-02 13:24:42 +0300

    add class with basics and some operations of compares

--- main.cpp
+++ main.cpp
@@ -2,14 +2,6 @@
 #include <iostream>
 
 int main(){
-	/*LooongLong l1("12345678909876543210123456789098765432101234567890987654321012345678909876543210");
-	LooongLong l2((unsigned long long) 999999999);
-	LooongLong l3((long long) -999999999);
-	std::cout<<"12345678909876543210123456789098765432101234567890987654321012345678909876543210"<<std::endl
-			 <<l1<<std::endl
-			 <<l2<<std::endl
-			 <<l3<<std::endl;
-	*/
 	std::string str;
 	std::cin>>str;
 	LooongLong l1(str);
@@ -17,6 +9,6 @@ int main(){
 	std::cin>>str;
 	LooongLong l2(str);
 	if(l1<l2)
-		cout<<l1<<" < "<<l2<<std::endl;
+		std::cout<<l1<<" < "<<l2<<std::endl;
 	return 0;
 }
