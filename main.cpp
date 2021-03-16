1508b3622afb9b7f9f6845f2e731fdbea4a50c19
student31415 <qwerty65k@mail.ru>
2021-03-16 17:06:00 +0300

    final commit

--- main.cpp
+++ main.cpp
@@ -3,20 +3,65 @@
 
 int main(){
 	std::string str;
-	std::cin>>str;
-	LooongLong l1(str);
+	while(true){
+		std::cin>>str;
+		if(!std::cin.good())
+			break;
+		LooongLong l1(str);
+		std::cin>>str;
+		LooongLong l2(str);
+		std::cin>>str;
+		if(str == "+"){
+			std::cout<<l1+l2<<std::endl;
+		}
+		if(str == "-"){
+			if(l1<l2)
+				std::cout<<"Error"<<std::endl;
+			else
+				std::cout<<l1-l2<<std::endl;
 
-	std::cin>>str;
-	LooongLong l2(str);
-	if(l1<l2)
-		std::cout<<l1<<" < "<<l2<<std::endl;
-	if(l1>l2)
-		std::cout<<l1<<" > "<<l2<<std::endl;
-	if(l1==l2)
-		std::cout<<l1<<" = "<<l2<<std::endl;
-	
-	std::cout<<l1+l2<<std::endl;
-	std::cout<<l1-l2<<std::endl;
-		
+		}
+		if(str == "*"){
+			std::cout<<l1*l2<<std::endl;
+		}
+		if(str == "/"){
+			try{
+				std::cout<<l1/l2<<std::endl;
+			}
+			catch(...){
+				std::cout<<"Error"<<std::endl;
+			}
+		}
+		if(str == "^"){
+			try{
+				std::cout<<l1.pow(l2)<<std::endl;
+			}
+			catch(...){
+				std::cout<<"Error"<<std::endl;
+			}
+		}
+		if(str == "<"){
+			if(l1<l2)
+				std::cout<<"true"<<std::endl;
+			else
+				std::cout<<"false"<<std::endl;
+
+		}
+		if(str == ">"){
+			if(l1>l2)
+				std::cout<<"true"<<std::endl;
+			else
+				std::cout<<"false"<<std::endl;
+
+		}
+		if(str == "="){
+			if(l1 == l2)
+				std::cout<<"true"<<std::endl;
+			else
+				std::cout<<"false"<<std::endl;
+
+		}
+
+	}		
 	return 0;
 }
